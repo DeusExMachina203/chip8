f14bfdb817c2d53b4a13c1ea2888e7e5c51a9d5c
DeusExMachina203 <ernestodeloayza@gmail.com>
2023-07-27 14:13:55 -0500

    first commit

---
+++ src/static/scripts/rederer.js
@@ -0,0 +1,34 @@
+class Renderer {
+  constructor(scale) {
+    this.cols = 64;
+    this.rows = 32;
+    this.scale = scale;
+
+    this.canvas = document.querySelector('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.canvas.width = this.cols * this.scale;
+    this.canvas.height = this.rows * this.scale;
+
+    this.display = new Array(this.cols * this.rows);
+  }
+  setPixel(x, y) {
+    if(x > this.cols){
+      x-=this.cols;
+    }else if(x< this.cols){
+      x+=this.cols;
+    }
+
+    if(y > this.rows){
+      y-=this.rows;
+    }else if(y< this.rows){
+      y+=this.rows;
+    }
+
+    let pixelLoc = x + (y* this.cols);
+
+    this.display[pixelLoc] ^= 1;
+  }
+}
+
+export default Renderer;
